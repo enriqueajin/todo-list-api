7fb4a974933ba2b50ceac3c35b52dc7a4c71a33c
Enrique <enriqueajin@outlook.com>
2023-11-23 11:27:15 -0600

    Feat add todo-controller users.

--- db.js
+++ db.js
@@ -1,11 +1,20 @@
 const Pool = require("pg").Pool;
+const { configDotenv } = require("dotenv");
+
+// Load configuration
+try {
+  configDotenv();
+} catch (e) {
+  console.error("Error loading configuration:", e);
+  process.exit(1);
+}
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "todo_list",
-    password: "root",
-    port: 5432
-})
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  port: process.env.DATABASE_PORT,
+});
 
 module.exports = pool;
--- src/todos/todoRoutes.js
+++ src/todos/todoRoutes.js
@@ -1,10 +1,10 @@
 const { Router } = require("express");
-const controller = require('./todoController')
+const controller = require("./todoController");
 
 const router = Router();
 
 router.get("/", controller.getTasks);
-router.get("/:id", controller.getTasksByUserId);
+router.get("/user/:id", controller.getTasksByUserId);
 router.post("/", controller.addTask);
 router.put("/:id", controller.updateTask);
 router.put("/:id", controller.updateTaskStatus);
